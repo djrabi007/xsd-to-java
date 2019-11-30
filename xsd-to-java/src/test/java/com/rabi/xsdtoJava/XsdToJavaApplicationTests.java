fdae19d7253a04f6b404bdda82f6d69153e296ec
“djrabi007” <rabi007007@gmail.com>
2019-11-30 21:48:31 +0530

    XSD to Java

---
+++ xsd-to-java/src/test/java/com/rabi/xsdtoJava/XsdToJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabi.xsdtoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsdToJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
